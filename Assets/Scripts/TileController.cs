ec19a7bbac588acc97c6f87ccb58c81113e348b9
Daniel Choi <daniel.won.choi@gmail.com>
2016-07-14 15:54:08 -0700

    crappy tile movement and title screen

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class TileController : MonoBehaviour {
 
+    public float distanceFromMiddle;
+
+    private Transform camera;
 	// Use this for initialization
 	void Start () {
-	
+        camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        transform.position = new Vector3(transform.position.x, camera.position.y - distanceFromMiddle, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
         }
