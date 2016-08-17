15dca24fc3797169d5f06bd3e305a2fe412d9e9a
Heesoo Home <heesoo4199@gmail.com>
2016-08-17 00:19:06 -0700

    Background Movement
    Subtle background movement (WIP) initial implementation.

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -5,6 +5,8 @@ public class BackgroundController : MonoBehaviour {
 
     public Sprite[] images = new Sprite[10];
 
+    float counter = 0f;
+
     // Use this for initialization
     void Start () {
         //Initializes background randomly to one of the ten
@@ -14,6 +16,12 @@ public class BackgroundController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //Implement scrolling
+        counter += Time.deltaTime;
+
+        float x = Mathf.Cos(counter / 10);
+        float y = Mathf.Sin(counter / 10);
+        
+        transform.position = new Vector3(8.69f * x, 2.5f * y + transform.parent.transform.position.y);
     }
 
 }
