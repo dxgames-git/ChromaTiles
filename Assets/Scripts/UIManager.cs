5d4fd3b4e773030ce08d1ae49258b0800dcb4a85
Heesoo Home <heesoo4199@gmail.com>
2016-08-28 22:58:42 -0700

    Minor Fix
    Last commit didn't go through with minor final changes with
    UIManager.cs, where I removed a redundant, unused field.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -16,47 +16,92 @@ public class UIManager : MonoBehaviour
     public AudioClip loopMusic;
     public AudioClip deathMusic;
 
-    private GameObject Box;
-
+    //Music controls
     private AudioSource gameMusic;
+    private float timeCounter;
+    private bool loopDone;
+    private bool deathDone;
 
     // Use this for initialization
     void Start()
     {
+        Time.timeScale = 1f;
         isPaused = false;
         isDead = false;
-        Box = GameObject.FindGameObjectWithTag("Box");
+
         gameMusic = gameObject.GetComponent<AudioSource>();
 
         timeCounter = 0f;
+        loopDone = false;
+        deathDone = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         timeCounter += Time.deltaTime;
+        //Computer-only: Press escape to toggle pause menu
         if (Input.GetButtonDown("Cancel"))
         {
             switchPause();
         }
+
+        //Loops music for gameplay
+        if (timeCounter >= introMusic.length && !loopDone)
         {
+            loopDone = true;
+            gameMusic.Stop();
+            gameMusic.clip = loopMusic;
+            gameMusic.PlayDelayed(0.75f);
         }
+
+        //Changes music for death screen
+        if (isDead && !deathDone)
         {
+            deathDone = true;
             deathPanel.SetActive(true);
+            gameMusic.Stop();
+            gameMusic.clip = deathMusic;
+            gameMusic.PlayDelayed(0.05f);
+
+            Time.timeScale = 0f;
         }
+        if (deathDone)
         {
+            Time.timeScale = 0f;
         }
     }
+
+    //Gets called whenever the pause button is pressed
     public void switchPause()
     {
+        if (!isPaused)
         {
+            isPaused = true; //Required for recognizing which toggle it is.
+            Time.timeScale = 0.0f; //Paused
+            gameMusic.Pause();
+            pausePanel.SetActive(true);
+            return;
         }
+        isPaused = false;
+        Time.timeScale = 1.0f; //Unpaused
+        gameMusic.UnPause();
+        pausePanel.SetActive(false);
     }
+
     public void restart()
     {
         gameMusic.Stop();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Start();
     }
+
     public void mainMenu()
     {
+        //Application.LoadLevel("Title Screen"); is outdated *Had to include "using UnityEngine.SceneManagement;" up top
+        gameMusic.Stop();
+        SceneManager.LoadScene("Title Screen");
+        Start();
     }
+
 }
