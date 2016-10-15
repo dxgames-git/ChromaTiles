2d76ff39ad448d818e2c2c60d2587f7439a30f40
Heesoo Home <heesoo4199@gmail.com>
2016-10-14 22:02:06 -0700

    New Title

---
+++ Assets/Scripts/Title Screen/TitleRing.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleRing : MonoBehaviour
+{
+
+    public Sprite image;
+    private int dir;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        gameObject.GetComponent<SpriteRenderer>().sprite = image;
+        dir = (int) (Random.value * 2f);
+        if (dir < 1)
+            dir = -1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(Vector3.forward * -20 * dir * Time.deltaTime);
+
+    }
+}
